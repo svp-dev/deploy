0b5c66badb9eefe569feb6606c9abc05d3b6a980
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-03-07 00:16:07 +0100

    Simplify the distribution Makefiles.
    * dist/common.mk (REQPARTS): Forget about SysC here too.

--- dist/common.mk
+++ dist/common.mk
@@ -3,7 +3,7 @@ META_SOURCES = sl/meta
 INST_SOURCES = sl/src
 INST_BUILD = sl/build
 
-REQPARTS = binutils m4 gcc sc slenv
+REQPARTS = binutils m4 gcc slenv
 SLPARTS = mgsim slc
 PARTS = $(REQPARTS) $(SLPARTS)
 
