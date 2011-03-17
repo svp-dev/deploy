acbfc73d024165425644a01e5d198f6801814a4e
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-03-17 15:07:21 +0100

    Update repository URL in packager.
    * dist/dist-common.mk (SLC_REPO): Change from `sys.git' to
    `slcore.git'.

--- dist/dist-common.mk
+++ dist/dist-common.mk
@@ -10,7 +10,7 @@ MKDIR_P = mkdir -p
 
 MGSIM_REPO = https://svn.svp-home.org/sw/svp/mg/mgsim
 BASE_REPO = https://git.svp-home.org
-SLC_REPO = $(BASE_REPO)/sys.git
+SLC_REPO = $(BASE_REPO)/slcore.git
 BINUTILS_REPO = $(BASE_REPO)/binutils.git
 DEPLOY_REPO = $(BASE_REPO)/deploy.git
 GCC_REPO = $(BASE_REPO)/gcc.git
