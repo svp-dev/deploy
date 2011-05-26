6e5ebfe2da91d4e7fa6cff0bbc3586de025b24c6
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-05-26 13:30:46 +0200

    Update the installer for MGSim 3.1.
    * installer/configure.ac: Check for SDL here.
    
    * installer/rules/mgsim.mk: Update to compile the simulator with
    different compile flags. This is necessary since 3.1 does not
    compile multiple binaries any more.
    
    * installer/rules/mgsim.mk: Move the command that generates the
    symbolic link to the target installation directory to...
    * installer/Makefile.am: ...here.

--- installer/rules/mgsim.mk
+++ installer/rules/mgsim.mk
@@ -14,21 +14,24 @@
 MGSIM_SRC = $(SRCBASE)/mgsim-$(MGSIM_VERSION)
 MGSIM_BUILD = $(BLDBASE)/mgsim-$(MGSIM_VERSION)
 
-MGSIM_CFG_TARGETS = $(MGSIM_BUILD)/configure_done
-MGSIM_BUILD_TARGETS = $(MGSIM_BUILD)/build_done
-MGSIM_INST_TARGETS = $(SLDIR)/.mgsim-installed
+MGSIM_TARGETS_BASE = opt-nosdl dbg-nosdl
+if ENABLE_SDL
+MGSIM_TARGETS_BASE += opt-sdl dbg-sdl
+endif
 
-MGSIM_TARGET_SELECT =
+MGSIM_TARGETS =
 if ENABLE_MTALPHA
-MGSIM_TARGET_SELECT += --enable-mtalpha
-else
-MGSIM_TARGET_SELECT += --disable-mtalpha
+MGSIM_TARGETS += $(foreach T,$(MGSIM_TARGETS_BASE),mtalpha-$(T))
 endif
 if ENABLE_MTSPARC
-MGSIM_TARGET_SELECT += --enable-mtsparc
-else
-MGSIM_TARGET_SELECT += --disable-mtsparc
+MGSIM_TARGETS += $(foreach T,$(MGSIM_TARGETS_BASE),mtsparc-$(T))
 endif
+
+MGSIM_CFG_TARGETS = $(foreach T,$(MGSIM_TARGETS),$(MGSIM_BUILD)-$(T)/configure_done)
+MGSIM_BUILD_TARGETS = $(foreach T,$(MGSIM_TARGETS),$(MGSIM_BUILD)-$(T)/build_done)
+MGSIM_INST_TARGETS = $(foreach T,$(MGSIM_TARGETS),$(SLDIR)/.mgsim-installed-$(T))
+
+MGSIM_CONFIG_FLAGS = --disable-abort-on-trace-failure --disable-verbose-trace-checks
 
 .PRECIOUS: $(MGSIM_ARCHIVE) $(MGSIM_CFG_TARGETS) $(MGSIM_BUILD_TARGETS) $(MGSIM_INST_TARGETS)
 
@@ -42,28 +45,40 @@ $(MGSIM_SRC)/configure: $(MGSIM_ARCHIVE)
 	$(UNTAR) $(SRCBASE) $(MGSIM_ARCHIVE)
 	touch $@
 
-$(MGSIM_BUILD)/configure_done: $(MGSIM_SRC)/configure $(SLTAG) $(BINUTILS_INST_TARGETS)
+$(MGSIM_BUILD)-%/configure_done: $(MGSIM_SRC)/configure $(SLTAG) $(BINUTILS_INST_TARGETS) $(REQCURRENT)
 	rm -f $@
-	$(MKDIR_P) $(MGSIM_BUILD)
-	cd $(DSTBASE) && rm -f slreqs-$(SLNAME) && $(LN_S) $(REQNAME) slreqs-$(SLNAME)
+	$(MKDIR_P) $(MGSIM_BUILD)-$*
+	mgsim_target=`echo $*|cut -d- -f1` && program_suffix=-`echo $$mgsim_target|cut -c3-` && \
+	mgsim_conf_flags=--target=$$mgsim_target && \
+	more_cflags= && \
+	case $* in \
+	  *-opt-*) mgsim_conf_flags="$$mgsim_conf_flags --disable-assert"; more_cflags="-O3 -g0" ;; \
+	  *-dbg-*) more_cflags="-O0 -g"; program_suffix=$$program_suffix.dbg ;; \
+	esac && \
+	case $* in \
+	  *-sdl) program_suffix=$$program_suffix.gfx ;; \
+	  *-nosdl) mgsim_conf_flags="$$mgsim_conf_flags --disable-sdl" ;; \
+	esac && \
 	SRC=$$(cd $(MGSIM_SRC) && pwd) && \
-	   cd $(MGSIM_BUILD) && \
+	   cd $(MGSIM_BUILD)-$* && \
 	   PATH=$(REQCURRENT)/bin:$$PATH $$SRC/configure --prefix=$(SLDIR) \
 			  CC="$(CC)" CXX="$(CXX)" \
 	                  CPPFLAGS="$(CPPFLAGS)" \
-			  CFLAGS="$(CFLAGS)" CXXFLAGS="$(CXXFLAGS)" \
+			  CFLAGS="$(CFLAGS) $$more_cflags" CXXFLAGS="$(CXXFLAGS) $$more_cflags" \
 	                  LDFLAGS="$(LDFLAGS)" \
-	                  $(MGSIM_TARGET_SELECT)
-	rm -f $(MGSIM_BUILD)/src/*main.o
+	                  $(MGSIM_CONFIG_FLAGS) \
+	                  --program-suffix=$$program_suffix \
+	                  $$mgsim_conf_flags
+	rm -f $(MGSIM_BUILD)-$*/src/*main.o
 	touch $@
 
-$(MGSIM_BUILD)/build_done: $(MGSIM_BUILD)/configure_done
+$(MGSIM_BUILD)-%/build_done: $(MGSIM_BUILD)-%/configure_done
 	rm -f $@
-	cd $(MGSIM_BUILD) && $(MAKE) all
+	cd $(MGSIM_BUILD)-$* && $(MAKE) all
 	touch $@
 
-$(SLDIR)/.mgsim-installed: $(MGSIM_BUILD)/build_done
+$(SLDIR)/.mgsim-installed-%: $(MGSIM_BUILD)-%/build_done
 	rm -f $@
-	cd $(MGSIM_BUILD) && $(MAKE) install
+	cd $(MGSIM_BUILD)-$* && $(MAKE) install
 	touch $@
 
