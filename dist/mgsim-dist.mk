6c4fec54adea226eee4c39448fb87017fdae6fed
Raphael 'kena' Poss <r.c.poss@uva.nl>
2011-05-26 13:31:57 +0200

    Update the packaging of MGSim to re-generate the ChangeLog.
    * dist/mgsim-dist.mk: Do it.

--- dist/mgsim-dist.mk
+++ dist/mgsim-dist.mk
@@ -19,8 +19,6 @@ MGSIM_DIST_VERSION = $(MGSIM_SVN_REV)-$(MGSIM_BASEBRANCH)
 MGSIM_DISTBASE = mgsim-$(MGSIM_DIST_VERSION)
 MGSIM_METASRC = $(META_SOURCES)/$(MGSIM_DISTBASE)
 
-MGSIM_PROGRAMS = mgsim-alpha mgsim-sparc
-
 $(MGSIM_METASRC)/download_done:
 	rm -f $@
 	$(MKDIR_P) $(META_SOURCES)
@@ -38,19 +36,19 @@ $(MGSIM_METASRC)/bootstrap_done: $(MGSIM_METASRC)/download_done
 # built systemc. This needs to be present on the system already!
 $(MGSIM_METASRC)/configure_done: $(MGSIM_METASRC)/bootstrap_done
 	rm -f $@
-	cd $(MGSIM_METASRC) && ./configure CPPFLAGS="$$CPPFLAGS" LDFLAGS="$$LDFLAGS"
+	cd $(MGSIM_METASRC) && ./configure --target=mtalpha CPPFLAGS="$$CPPFLAGS" LDFLAGS="$$LDFLAGS"
 	touch $@
 
 $(MGSIM_METASRC)/build1_done: $(MGSIM_METASRC)/configure_done
 	rm -f $@
-	cd $(MGSIM_METASRC) && $(MAKE) .version && $(MAKE) -C src $(MGSIM_PROGRAMS)
+	cd $(MGSIM_METASRC) && $(MAKE) .version && $(MAKE) mgsim
 	touch $@
 
 .PHONY: mgsim-dist
 mgsim-dist: $(MGSIM_METASRC)/build1_done
 	$(MKDIR_P) $(ARCHIVEDIR)
 	rm -f $(MGSIM_METASRC)/*.tar.*
-	cd $(MGSIM_METASRC) && make dist
+	cd $(MGSIM_METASRC) && rm -f ChangeLog && make ChangeLog && make dist
 	for arch in $(MGSIM_METASRC)/*.tar.bz2; do \
 	   bn=`basename $$arch`; \
 	   if ! test -f $(ARCHIVEDIR)/$$bn; then \
